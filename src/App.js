d203166255a34e97fed5606b76571e06e03f7596
Fernando Fernandes <fernando.fernandes.opah.com.br>
2023-01-27 20:14:47 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useLayoutEffect, useReducer} from 'react';
+var userDetailContext = React.createContext(null);
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [a, setA] = useState(0);
+ 
+
+
+
+  useLayoutEffect(() => {
+    setA(count + 1)
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+       <userDetailContext.Provider value={a}>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      {count}
+      <App2></App2>
+      </userDetailContext.Provider>
+      <App3></App3>
+      <App4></App4>
     </div>
   );
 }
 
-export default App;
+
+const App2 = () => {
+  var contextData = React.useContext(userDetailContext);
+  return (
+    <div>
+      esse viu -> {contextData}
+       
+    </div>
+  )
+}
+
+const App3 = () => {
+  const [randomNumber, setRandomNumber] = useState(0)
+  const [effectLogs, setEffectLogs] = useState([])
+  
+  useLayoutEffect(
+    () => {
+      setEffectLogs(prevEffectLogs => [...prevEffectLogs, 'funcionouuuu'])
+    },
+    [randomNumber]
+  )
+  return (
+    <div>
+    <h1>{randomNumber}</h1>
+    <button
+      onClick={() => {
+        setRandomNumber(Math.random())
+      }}
+    >
+     Gerar númeroa aletatórios
+    </button>
+    <div>
+      {effectLogs.map((effect, index) => (
+        <div key={index}>{'1'.repeat(index) + effect}</div>
+      ))}
+    </div>
+  </div>
+  )
+}
+
+const App4 = () =>{
+  const initialState = { width: 15 };
+
+const reducer2 = (state, action) => {
+  switch (action) {
+    case 'plus':
+      return { width: state.width + 15 }
+    case 'minus':
+      return { width: Math.max(state.width - 15, 2) }
+    default:
+      throw new Error("what's going on?" )
+  }
+}
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const [state, dispatch] = useReducer(reducer2, initialState)
+
+  return (
+    <>
+        <div style={{ background: 'teal', height: '30px', width: state.width }}></div>
+    <div style={{marginTop: '3rem'}}>
+        <button onClick={() => dispatch('plus')}>Aumenta barra</button>
+        <button onClick={() => dispatch('minus')}>Diminui Barra</button>
+    </div>
+
+    </>
+  )
+}
+
+export default App
